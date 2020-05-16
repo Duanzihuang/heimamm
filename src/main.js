ab26a496d22cbb906b2795f87fb2126ea891369e
manageritcast <manageritcast@126.com>
2020-05-16 15:56:34 +0800

    2-创建了项目所需要的文件及文件夹

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import axios from 'axios'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 导入less
+import '@/styles/base.less'
 
 new Vue({
   render: h => h(App),
