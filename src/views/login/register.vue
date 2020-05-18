81cf3f5d5be9e54b2e533dba62eb33b79c9bd2e9
manageritcast <manageritcast@126.com>
2020-05-18 11:55:26 +0800

    10-完成了注册页面的布局

--- src/views/login/register.vue
+++ src/views/login/register.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>
-    <el-dialog title="注册" width="600px" :visible.sync="dialogVisible">
-      <span>这个是测试dialog</span>
+  <div class="register">
+    <el-dialog width="600px" center :visible.sync="dialogVisible">
+      <div slot="title" class="title">
+        注册
+      </div>
+      <el-form :model="resgisterForm" label-width="100px">
+        <el-form-item label="昵称">
+          <el-input v-model="resgisterForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="resgisterForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机">
+          <el-input v-model="resgisterForm.phone"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="resgisterForm.password" show-password></el-input>
+        </el-form-item>
+        <el-form-item label="图形码">
+          <el-row>
+            <el-col :span="17">
+              <el-input></el-input>
+            </el-col>
+            <el-col style="margin-left:10px" :span="6">
+              <img
+                class="captcha"
+                src="http://47.106.148.205/heimamm/public/captcha?type=sendsms&t=1589773089302"
+                alt=""
+              />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item label="验证码">
+          <el-row>
+            <el-col :span="17">
+              <el-input v-model="resgisterForm.rcode"></el-input>
+            </el-col>
+            <el-col style="margin-left:10px" :span="6">
+              <el-button>获取用户验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
       <span slot="footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button @click="dialogVisible = false" type="primary"
@@ -21,6 +61,15 @@ export default {
       //   dialogVisible: this.isShow,
 
       dialogVisible: false,
+      resgisterForm: {
+        // 这里面的数据，将来是要提交给服务器
+        username: "", // 用户名
+        phone: "", // 手机号
+        email: "", // 邮箱
+        avatar: "", // 头像地址
+        password: "", // 密码
+        rcode: "", // 验证码
+      },
     };
   },
   //   watch: {
@@ -30,3 +79,25 @@ export default {
   //   }
 };
 </script>
+
+<style lang="less">
+.register {
+  .title {
+    height: 53px;
+    background: #03c0f9;
+    color: #fff;
+    text-align: center;
+    line-height: 53px;
+  }
+  .el-dialog__header {
+    padding: 0px;
+  }
+  .el-dialog__headerbtn .el-dialog__close {
+    color: #fff;
+  }
+  .captcha {
+    width: 100%;
+    height: 40px;
+  }
+}
+</style>
