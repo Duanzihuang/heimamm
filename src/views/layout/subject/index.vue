75d05aec4b1f0371c93ea38d1b22f2ae8cb2e714
manageritcast <manageritcast@126.com>
2020-05-24 17:00:07 +0800

    30-完成了学科列表组件搜索功能

--- src/views/layout/subject/index.vue
+++ src/views/layout/subject/index.vue
@@ -1,5 +1,92 @@
 <template>
-    <div>
-        subject
-    </div>
+  <div>
+    <!-- 搜索部分 -->
+    <el-card>
+      <el-form
+        inline
+        :model="searchForm"
+        ref="searchFormRef"
+        label-width="80px"
+      >
+        <el-form-item label="学科编号" prop="rid">
+          <el-input style="width:180px;" v-model="searchForm.rid"></el-input>
+        </el-form-item>
+        <el-form-item label="学科姓名" prop="name">
+          <el-input style="width:180px;" v-model="searchForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="创建者" prop="username">
+          <el-input
+            style="width:180px;"
+            v-model="searchForm.username"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="状态" prop="status">
+          <el-select
+            style="width:180px;"
+            v-model="searchForm.status"
+            placeholder="请选择"
+          >
+            <el-option label="启用" value="1"></el-option>
+            <el-option label="禁用" value="0"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="search" type="primary">搜索</el-button>
+          <el-button @click="clear">清除</el-button>
+          <el-button type="primary">+新增学科</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 列表、分页部分 -->
+    <el-card style="margin-top:15px;"> </el-card>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Subject",
+  data() {
+    return {
+      searchForm: {
+        rid: "", // 学科编号
+        name: "", // 学科姓名
+        username: "", // 创建者
+        status: "", // 创建者
+      },
+      page: 1, // 页码
+      limit: 2, // 页容量
+      subjectList: [], // 学科列表数据
+      total: 0, // 总条数
+    };
+  },
+  created() {
+      this.getSubjectListData()
+  },
+  methods: {
+    async getSubjectListData() {
+      const res = await this.$axios.get("/subject/list", {
+        params: {
+          page: this.page,
+          limit: this.limit,
+          ...this.searchForm
+        },
+      });
+
+      if (res.data.code === 200) {
+        this.subjectList = res.data.data.items;
+        this.total = res.data.data.pagination.total;
+      }
+    },
+    search() {
+        this.page = 1
+
+        this.getSubjectListData()
+    },
+    clear() {
+        this.$refs.searchFormRef.resetFields()
+
+        this.search()
+    }
+  },
+};
+</script>
