154997c1f2d289326e6a0888c0b4bc3548358a42
manageritcast <manageritcast@126.com>
2020-05-26 17:07:55 +0800

    40-新增题目渲染城市完成

--- src/views/layout/question/question-add-or-update.vue
+++ src/views/layout/question/question-add-or-update.vue
@@ -27,9 +27,68 @@
             </el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="阶段" prop="step">
+          <el-select
+            class="selectWidth"
+            v-model="questionForm.step"
+            placeholder="请选择阶段"
+          >
+            <el-option
+              v-for="(value, name) in stepObj"
+              :key="name"
+              :label="value"
+              :value="name"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="企业" prop="enterprise">
+          <el-select
+            class="selectWidth"
+            v-model="questionForm.enterprise"
+            placeholder="请选择企业"
+          >
+            <el-option
+              v-for="item in enterpriseList"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="城市" prop="city">
+          <el-cascader
+            size="large"
+            :options="options"
+            :props="{ value: 'label' }"
+            v-model="questionForm.city"
+          >
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="题型" prop="type">
+          <el-radio-group v-model="questionForm.type">
+            <el-radio
+              v-for="(value, name) in typeObj"
+              :key="name"
+              :label="name"
+              >{{ value }}</el-radio
+            >
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="难度" prop="difficulty">
+          <el-radio-group v-model="questionForm.difficulty">
+            <el-radio
+              v-for="(value, name) in difficultyObj"
+              :key="name"
+              :label="name"
+              >{{ value }}</el-radio
+            >
+          </el-radio-group>
+        </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
           <el-button>取消</el-button>
+          <el-button type="primary" @click="submit">确定</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -37,9 +96,13 @@
 </template>
 
 <script>
+import { regionData } from "element-china-area-data";
 export default {
   name: "QuestionEdit",
   //   props: ['subjectList','enterpriseList'],
+  created() {
+    console.log(regionData);
+  },
   props: {
     subjectList: Array,
     enterpriseList: Array,
@@ -51,14 +114,33 @@ export default {
     return {
       mode: "", // add 新增 edit 修改
       dialogVisible: false, // 控制dialog的显示及隐藏
+      options: regionData,
       questionForm: {
         // 这个里面的所有值，将来是传递给服务器的
-        subject: "",
+        subject: "", // 学科
+        step: "", // 阶段
+        enterprise: "", // 企业
+        city: [], // 城市
+        type: "1", // 题型
+        difficulty: "1", // 难度
       },
       rules: {
         subject: [{ required: true, message: "请选择学科", trigger: "change" }],
+        step: [{ required: true, message: "请选择阶段", trigger: "change" }],
+        enterprise: [
+          { required: true, message: "请选择企业", trigger: "change" },
+        ],
+        city: [{ required: true, message: "请选择城市", trigger: "change" }],
+        type: [{ required: true, message: "请选择题型", trigger: "change" }],
+        difficulty: [
+          { required: true, message: "请选择难度", trigger: "change" },
+        ],
       },
     };
+  },
+  methods: {
+    // 新增 & 修改
+    submit() {},
   },
 };
 </script>
